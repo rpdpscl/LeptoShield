4cb09c9ca26a3fd9f0d8a07365d71092a2b1a0cb
Ron Pascual <rpdpscl@gmail.com>
2024-08-18 07:45:03 +0800

    Update lepto_app.py

--- lepto_app.py
+++ lepto_app.py
@@ -125,7 +125,7 @@ if 'lepto_df' in locals() and not lepto_df.empty:
          # Display the markdown content
         st.markdown(description)
 
-            st.title("City Insights")
+        st.title("City Insights")
         # Arrange the selectors side by side
         col1, col2 = st.columns(2)
         with col1:

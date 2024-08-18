a5d833bd0bd3e1759c00f9258ce4df1d836e1620
Ron Pascual <rpdpscl@gmail.com>
2024-08-18 22:06:37 +0800

    Update lepto_app.py

--- lepto_app.py
+++ lepto_app.py
@@ -21,13 +21,88 @@ plt.rcParams.update({
     'axes.titlecolor': 'gray',
 })
 
+# Add custom CSS for Streamlit theme with adjustments for title, description, and spacing
+st.markdown("""
+    <style>
+    .main {
+        background-color: white;
+    }
+    .sidebar .sidebar-content {
+        background-color: #f0f0f0;
+    }
+    .stButton>button {
+        background-color: #19535b;
+        color: white;
+    }
+    .stRadio label, .stSelectbox label {
+        color: #3d3d3d;  /* Set the color to #3d3d3d */
+        font-size: 4px;  /* Match font size with dropdowns */
+        margin-bottom: 0px;  /* Bring labels closer to the dropdown */
+    }
+    .css-18e3th9 {
+        color: white !important;
+    }
+    h1, p, h2, h3 {
+        font-family: 'Arial', sans-serif;
+    }
+    h1 {
+        color: #19535b !important;
+        font-size: 36px;
+        margin-top: -5px;  /* Reduced top margin to remove extra space above LeptoShield */
+        margin-bottom: 0px;
+        text-align: center;
+    }
+    p {
+        color: #19535b;
+        font-size: 10px;
+        line-height: 1.4;
+        margin-bottom: 5px;
+        text-align: center;
+    }
+    h2 {
+        color: #ffffff;
+        font-size: 25px;
+        margin-top: 10px;
+        background-color: #19535b;
+        padding: 2px 8px;
+        padding-left: 25px;
+        display: inline-block;
+        border-radius: 3px;
+    }
+    h3 {
+        text-decoration: underline;
+        font-weight: bold;
+        margin-top: 0px;  /* Reduce space above City Name */
+        font-size: 14px;
+        color: #3d3d3d;
+    }
+    .info {
+        font-size: 14px;
+        margin-top: -10px;  /* Reduce space between dropdowns and city info */
+        margin-bottom: 0px;
+        line-height: 1.6;
+        color: #3d3d3d; /* Matching the color of the dropdowns */
+    }
+    .stMarkdown {
+        margin-bottom: 0px;
+    }
+    .stSelectbox > div > div {
+        background-color: #d9d9d9;  /* Set dropdown background color */
+        color: #3d3d3d;  /* Set dropdown text color */
+        height: 35px;  /* Slightly increased height to prevent text from being cropped */
+        font-size: 12px;  /* Consistent font size inside dropdowns */
+        border: none;  /* Remove the border */
+        box-shadow: none;  /* Remove any shadow that may appear as a border */
+    }
+
+    </style>
+""", unsafe_allow_html=True)
+
 # Load your dataset and handle errors
 try:
     lepto_df = pd.read_csv('lepto_dfclean.csv')
-    city_summary = pd.read_csv('city_summary.csv')
-    
-    if lepto_df.empty or city_summary.empty:
-        st.error("One or more datasets are empty. Please check the CSV files.")
+    if lepto_df.empty:
+        st.error("The dataset is empty. Please check the CSV file.")
     else:
         lepto_df['date'] = pd.to_datetime(lepto_df['date'])
         lepto_df['month'] = lepto_df['date'].dt.month
@@ -35,13 +110,13 @@ try:
         lepto_df['week'] = lepto_df['date'].dt.isocalendar().week
 
 except FileNotFoundError:
-    st.error("One or more files were not found. Please upload the correct files and ensure the paths are correct.")
+    st.error("The file 'lepto_dfclean.csv' was not found. Please upload the correct file and ensure the path is correct.")
 except pd.errors.EmptyDataError:
-    st.error("One or more files are empty. Please check the contents of the files.")
+    st.error("The file 'lepto_dfclean.csv' is empty. Please check the contents of the file.")
 except Exception as e:
     st.error(f"An unexpected error occurred: {e}")
 
-if 'lepto_df' in locals() and not lepto_df.empty and 'city_summary' in locals() and not city_summary.empty:
+if 'lepto_df' in locals() and not lepto_df.empty:
     def main():
         st.title("LeptoShield")
         # Display the app description and disclaimer
@@ -61,23 +136,10 @@ if 'lepto_df' in locals() and not lepto_df.empty and 'city_summary' in locals() 
             
         # Filter data for the selected city
         city_data = lepto_df[lepto_df['adm3_en'] == selected_city]
-        city_info = city_summary[city_summary['adm3_en'] == selected_city].iloc[0]  # Get city-specific information
         
-        # Update placeholders with actual data from city_summary
-        city_name = city_info['adm3_en']
-        city_area = city_info['city_area']
-        total_population = city_info['pop_count_total']
-        population_density = city_info['pop_density']
-        total_cases = city_info['case_total']
-
-        # Display the city-specific information
-        st.markdown(f"### {city_name}")
-        st.markdown(f"""
-        <div class='info'><b>Total Area:</b> {city_area} sq km<br>
-        <b>Total Population:</b> {total_population}<br>
-        <b>Population Density:</b> {population_density} persons per sq km<br>
-        <b>Total Number of Recorded Cases:</b> {total_cases} (2008-2020)</div>
-        """, unsafe_allow_html=True)
+        # Placeholder for city-specific information
+        st.markdown("### City Name (Placeholder)")
+        st.markdown("<div class='info'><b>Total Area:</b> (Placeholder)<br><b>Total Population:</b> Population as of (date) (Placeholder)<br><b>Population Density:</b> Total Pop/Total Area (Placeholder) persons per sqkm<br><b>Total Number of Recorded Cases:</b> (Number) (2008-2020) (Placeholder)</div>", unsafe_allow_html=True)
 
         # Placeholder for Leptospirosis Cases Summary
         st.markdown("### Leptospirosis Cases Summary")
@@ -124,7 +186,7 @@ if 'lepto_df' in locals() and not lepto_df.empty and 'city_summary' in locals() 
             ]
             
             # Joining month names into a readable string
-            top_months_str = ", ".join(top_months_names[:-1]) + ", and " + top_month_names[-1]
+            top_months_str = ", ".join(top_month_names[:-1]) + ", and " + top_month_names[-1]
         
             # Plotting the data
             fig, ax = plt.subplots(figsize=fig_size)
@@ -158,6 +220,18 @@ if 'lepto_df' in locals() and not lepto_df.empty and 'city_summary' in locals() 
             ax.bar(weekly_counts['case_category'], weekly_counts['count'], color=['#19535b', '#d9d9d9'])
             ax.set_title('No. of Weeks With/Without Cases', fontsize=14, color='gray')
             st.pyplot(fig)
+
+        # Layout for 3 columns
+        col1, col2, col3 = st.columns(3)
+
+        # Set uniform figure size
+        fig_size = (4, 4)
+        with col1:
+            st.markdown(f"The total number of cases peaked at **{max_cases}** in **{max_year}**")
+        with col2:
+            st.markdown(f"The average monthly cases peaked at **{int(peak_cases)}** and were highest in **{top_months_str}**.")
+        with col3:
+            st.markdown(f"From 2008-2020, there were {with_case_count} weeks **with cases** and {without_case_count} **weeks without cases**.")
 
         # Placeholder for Leptospirosis Cases Summary
         st.markdown("### Leptospirosis Risk Factors")
@@ -204,15 +278,16 @@ if 'lepto_df' in locals() and not lepto_df.empty and 'city_summary' in locals() 
             # Setting up x-axis labels and title
             ax.set_xticks(range(1, 13))
             ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], fontsize=8)
-            ax.set_title(f'Overlay of {feature.replace("_", " ").title()} with Average Monthly Cases', fontsize=14, color='gray')
+            ax.set_title(f'Cases vs {feature.replace("_", " ").title()}', fontsize=10, color='gray')
             ax.legend(fontsize=8)
         
             # Displaying the plot
             st.pyplot(fig)
 
-        # Placeholder for the second column in the second row
+        
+        # Placeholder for the second column
         with col2:
-            # Placeholder for future visualization
+            # You can use st.empty() or a simple text placeholder
             st.markdown("### Placeholder for Future Visualization")
             
     if __name__ == "__main__":
